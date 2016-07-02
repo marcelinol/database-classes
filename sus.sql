05a94fdda932fd817dc379a9a42f1c60e1a71390
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-07-02 17:12:44 -0300

    Fix order to avoid errors when creating tables

--- sus.sql
+++ sus.sql
@@ -42,6 +42,11 @@ CREATE TABLE Profissional (
   IdProfissional SERIAL PRIMARY KEY
 );
 
+CREATE TABLE Clinica (
+  Nome VARCHAR(30),
+  CodigoClinica SERIAL PRIMARY KEY
+);
+
 CREATE TABLE Seguradora (
   CNPJ VARCHAR(14) PRIMARY KEY,
   Nome VARCHAR(30)
@@ -50,6 +55,45 @@ CREATE TABLE Seguradora (
 CREATE TABLE Empregadora (
   CNPJ VARCHAR(14) PRIMARY KEY,
   CNAE VARCHAR(7),
+  Nome VARCHAR(30)
+);
+
+CREATE TYPE genero AS ENUM('masculino', 'feminino');
+CREATE TABLE Paciente (
+  Nome VARCHAR(50),
+  Sexo genero,
+  Raca VARCHAR(10),
+  Etnia VARCHAR(10),
+  TelefoneExtra VARCHAR(10),
+  NomeResponsavel VARCHAR(50),
+  Telefone VARCHAR(13),
+  DataNascimento DATE,
+  IdPaciente SERIAL PRIMARY KEY,
+  CNS VARCHAR(15),
+  NumeroProntuario VARCHAR(20),
+  NomeMae VARCHAR(50),
+  NumeroResidencia VARCHAR(10),
+  ComplementoResidencia VARCHAR(20),
+  CEP VARCHAR(10),
+  FOREIGN KEY(CEP) REFERENCES Logradouro (CEP) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE TABLE Doenca (
+  Descricao VARCHAR(30),
+  Cid VARCHAR(6) PRIMARY KEY
+);
+
+CREATE TABLE Procedimento (
+  CodigoProcedimento SERIAL PRIMARY KEY,
+  Descricao VARCHAR(20),
+  CodigoClinica SERIAL,
+  IdProfissional SERIAL,
+  FOREIGN KEY(CodigoClinica) REFERENCES Clinica (CodigoClinica)ON UPDATE RESTRICT ON DELETE RESTRICT,
+  FOREIGN KEY(IdProfissional) REFERENCES Profissional (IdProfissional) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+CREATE TABLE Estabelecimento (
+  CNES VARCHAR(7) PRIMARY KEY,
   Nome VARCHAR(30)
 );
 
@@ -78,6 +122,27 @@ CREATE TABLE Internacao (
   FOREIGN KEY(CNESExecutor) REFERENCES Estabelecimento (CNES) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
+CREATE TABLE CidSecundario (
+  Cid VARCHAR(6),
+  IdInternacao INTEGER,
+  FOREIGN KEY(Cid) REFERENCES Doenca (Cid),
+  FOREIGN KEY(IdInternacao) REFERENCES Internacao (IdInternacao) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+CREATE TABLE CidCausas (
+  Cid VARCHAR(6),
+  IdInternacao SERIAL,
+  FOREIGN KEY(Cid) REFERENCES Doenca (Cid) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  FOREIGN KEY(IdInternacao) REFERENCES Internacao (IdInternacao) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+CREATE TABLE ExamesDaInternacao (
+  IdExame SERIAL,
+  IdInternacao SERIAL,
+  FOREIGN KEY(IdExame) REFERENCES Exame (IdExame) ON UPDATE RESTRICT ON DELETE RESTRICT,
+  FOREIGN KEY(IdInternacao) REFERENCES Internacao (IdInternacao) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
 CREATE TYPE vinculoprev AS ENUM('empregado', 'empregador', 'autonomo', 'desempregado', 'aposentado', 'nao segurado');
 CREATE TYPE tipoacidente AS ENUM('transito', 'trabalho tipico', 'trabalho trajeto');
 CREATE TABLE Acidente (
@@ -93,69 +158,4 @@ CREATE TABLE Acidente (
   FOREIGN KEY(CNPJSeguradora) REFERENCES Seguradora (CNPJ) ON DELETE RESTRICT ON UPDATE RESTRICT,
   FOREIGN KEY(CNPJEmpregadora) REFERENCES Empregadora (CNPJ) ON DELETE RESTRICT ON UPDATE RESTRICT,
   FOREIGN KEY(IdInternacao) REFERENCES Internacao (IdInternacao) ON DELETE RESTRICT ON UPDATE RESTRICT
-);
-
-CREATE TYPE genero AS ENUM('masculino', 'feminino')
-CREATE TABLE Paciente (
-  Nome VARCHAR(50),
-  Sexo genero,
-  Raca VARCHAR(10),
-  Etnia VARCHAR(10),
-  TelefoneExtra VARCHAR(10),
-  NomeResponsavel VARCHAR(50),
-  Telefone VARCHAR(13),
-  DataNascimento DATE,
-  IdPaciente SERIAL PRIMARY KEY,
-  CNS VARCHAR(15),
-  NumeroProntuario VARCHAR(20),
-  NomeMae VARCHAR(50),
-  NumeroResidencia VARCHAR(10),
-  ComplementoResidencia VARCHAR(20),
-  CEP VARCHAR(10),
-  FOREIGN KEY(CEP) REFERENCES Logradouro (CEP) ON DELETE RESTRICT ON UPDATE RESTRICT
-);
-
-CREATE TABLE Estabelecimento (
-  CNES VARCHAR(7) PRIMARY KEY,
-  Nome VARCHAR(30)
-);
-
-CREATE TABLE ExamesDaInternacao (
-  IdExame SERIAL,
-  IdInternacao SERIAL,
-  FOREIGN KEY(IdExame) REFERENCES Exame (IdExame) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  FOREIGN KEY(IdInternacao) REFERENCES Internacao (IdInternacao) ON UPDATE RESTRICT ON DELETE RESTRICT
-);
-
-CREATE TABLE Doenca (
-  Descricao VARCHAR(30),
-  Cid VARCHAR(6) PRIMARY KEY
-);
-
-CREATE TABLE CidSecundario (
-  Cid VARCHAR(6),
-  IdInternacao INTEGER,
-  FOREIGN KEY(Cid) REFERENCES Doenca (Cid),
-  FOREIGN KEY(NumeroAutorizacao,IdInternacao) REFERENCES Internacao (NumeroAutorizacao,IdInternacao)
-);
-
-CREATE TABLE CidCausas (
-  Cid VARCHAR(6),
-  IdInternacao SERIAL,
-  FOREIGN KEY(Cid) REFERENCES Doenca (Cid) ON UPDATE RESTRICT ON DELETE RESTRICT,
-  FOREIGN KEY(IdInternacao) REFERENCES Internacao (IdInternacao) ON UPDATE RESTRICT ON DELETE RESTRICT
-);
-
-CREATE TABLE Clinica (
-  Nome VARCHAR(30),
-  CodigoClinica SERIAL PRIMARY KEY
-);
-
-CREATE TABLE Procedimento (
-  CodigoProcedimento SERIAL PRIMARY KEY,
-  Descricao VARCHAR(20),
-  CodigoClinica SERIAL,
-  IdProfissional SERIAL,
-  FOREIGN KEY(CodigoClinica) REFERENCES Clinica (CodigoClinica)ON UPDATE RESTRICT ON DELETE RESTRICT,
-  FOREIGN KEY(IdProfissional) REFERENCES Profissional (IdProfissional) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
