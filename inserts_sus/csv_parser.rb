50bcab551ba767c64a9cbbaa89b7517f5f70cefc
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-07-03 19:07:18 -0300

    ruby parser more generic

--- inserts_sus/csv_parser.rb
+++ inserts_sus/csv_parser.rb
@@ -1,9 +1,16 @@
 require 'csv'
 require 'pry'
 
-table = 'estabelecimento'
+def parse_file(file_name, table_name = nil)
+  table_name ||= file_name.chomp('.csv')
 
-CSV.parse(File.read("#{table}.csv"), headers: true) do |row|
-  values = row.fields.map { |f| "'#{f}'" }.join(',')
-  puts "insert into #{table}(#{row.headers.join(',')}) values(#{values});"
+  CSV.parse(File.read("#{file_name}"), headers: true) do |row|
+    values = row.fields.map { |f| "'#{f}'" }.join(',')
+    puts "insert into #{table_name}(#{row.headers.join(',')}) values(#{values});"
+  end
 end
+
+
+files = Dir.entries('.').map { |file| file if file.match(/\.csv/) }.compact!
+
+files.each { |file| parse_file(file) }
