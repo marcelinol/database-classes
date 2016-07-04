ee4d4a10f7269d8a4086e46f58640e456973d578
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-07-03 23:09:01 -0300

    improve csv parser and add values to be inserted

--- inserts_sus/csv_parser.rb
+++ inserts_sus/csv_parser.rb
@@ -1,16 +1,17 @@
 require 'csv'
 require 'pry'
 
-def parse_file(file_name, table_name = nil)
-  table_name ||= file_name.chomp('.csv')
+def parse_file(file_name)
+  table_name ||= file_name.match(/[a-zA-Z].*/)[0].chomp('.csv')
 
   CSV.parse(File.read("#{file_name}"), headers: true) do |row|
+    file = File.open('inserts.sql', 'a')
     values = row.fields.map { |f| "'#{f}'" }.join(',')
-    puts "insert into #{table_name}(#{row.headers.join(',')}) values(#{values});"
+    file.puts "insert into #{table_name}(#{row.headers.join(',')}) values(#{values});"
   end
 end
 
 
 files = Dir.entries('.').map { |file| file if file.match(/\.csv/) }.compact!
 
-files.each { |file| parse_file(file) }
+files.sort.each { |file| parse_file(file) }
